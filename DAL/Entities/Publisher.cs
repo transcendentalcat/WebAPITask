214ee3a3af4845075cce93abcbffebd8d758bc5d
YourNina Nazarova <Nina_Nazarova@epam.com>
2018-06-20 00:39:05 +0300

    Entities: Publisher

---
+++ DAL/Entities/Publisher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Publisher
+    {
+        string Key { get; set;}
+	    string Name { get; set;}
+        public ICollection<Game> Games { get; set; }
+    }
+
+}
+}
