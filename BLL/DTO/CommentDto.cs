73d2770d4423ad73037cc4e461a0a6aee9e02a09
Your Name <Nina_Nazarova@epam.com>
2018-06-20 17:39:55 +0300

    DTOs

---
+++ BLL/DTO/CommentDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace BLL.DTO
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public int GameId { get; set; }
+        public Comment ParentComment { get; set; }
+    }
+}
---
+++ BLL/DTO/GenreDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace BLL.DTO
+{
+    public class GenreDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Genre Subgenre { get; set; }
+    }
+}
