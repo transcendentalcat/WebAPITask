93a15dda3079760f9c63def7f7a49e37486cbc6f
Your Name <Nina_Nazarova@epam.com>
2018-06-20 16:38:26 +0300

    IUnitOfWork

--- DAL/Interfaces/IUnitOfWork.cs
+++ DAL/Interfaces/IUnitOfWork.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Entities;
 
 namespace DAL.Interfaces
 {
-    interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
+
     {
+
+        IRepository<Comment> Comments { get; }
+
+        IRepository<Game> Games { get; }
+
+        IRepository<Genre> Genres { get; }
+
+        IRepository<PlatformType> PlatformTypes { get; }
+
+        IRepository<Publisher> Publishers { get; }
+
+        Task SaveAsync();
+
     }
 }
